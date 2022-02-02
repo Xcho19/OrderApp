e72f3d0ed5e2df12f21ac6c65bf8c7aa535c5231
Xcho <xach55@gmail.com>
2022-02-03 01:29:27 +0400

    Add part 3

---
+++ OrderApp/ResponseModels.swift
@@ -0,0 +1,32 @@
+//
+//  ResponseModels.swift
+//  OrderApp
+//
+//  Created by Xcho on 03.02.22.
+//
+
+import Foundation
+
+struct MenuResponse: Codable {
+    let items: [MenuItem]
+}
+
+struct CategoriesResponse: Codable {
+    let categories: [String]
+}
+
+struct OrderResponse: Codable {
+    let prepTime: Int
+    
+    enum CodingKeys: String, CodingKeys {
+        case prepTime = "preperation_time"
+    }
+}
+
+struct Order: Codable {
+    var menuItems: [MenuItem]
+    
+    init(menuItems: [MenuItem] = []) {
+        self.menuItems = menuItems
+    }
+}
